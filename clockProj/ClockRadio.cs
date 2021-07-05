b8a8b1b49a3d637b8495be3f4956f07a8b36526f
Telvin Mathews <telvinmathews@gmail.com>
2021-07-04 19:17:24 -0500

    finished clock radio proj

---
+++ clockProj/ClockRadio.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace clockProj
+{
+    public class ClockRadio
+    {
+            //member variables
+            public string currentTime;
+            public string currentStation;
+            public bool alarmStatus;
+            public string alarmTime;
+
+            //constructor
+            public ClockRadio(string currenTime, string currentStation, bool alarmStatus, string alarmTime)
+            {
+                this.currentTime = currenTime;
+                this.currentStation = currentStation;
+                this.alarmStatus = alarmStatus;
+                this.alarmTime = alarmTime;
+            }
+
+            //member methods
+            public string GetCurrentTime()
+            {
+            Console.WriteLine($"Current time is: {currentTime}");
+            return currentTime;
+            }
+
+            public void ChangeCurrentTime()
+        {
+            Console.WriteLine("Change current time. ");
+            currentTime = Console.ReadLine();
+        }
+
+            public string GetCurrentStaion()
+        {
+            Console.WriteLine($"Current station is: {currentStation}");
+            return currentStation;
+        }
+
+            public void ChangeStation()
+        {
+            Console.WriteLine("Change station. ");
+            currentStation = Console.ReadLine();
+        }
+
+        public string GetAlarmTime()
+        {
+            Console.WriteLine($"Current alarm time is: {alarmTime}");
+            return alarmTime;
+        }
+
+        public void ChangeAlarmTime()
+        {
+            Console.WriteLine("Change alarm time. ");
+            alarmTime = Console.ReadLine();
+        }
+
+        public bool AlarmOn()
+        {
+           return alarmStatus = true;
+        }
+
+        public bool AlarmOff()
+        {
+            return alarmStatus = false; 
+        }
+            
+
+
+
+        }
+    }
---
+++ clockProj/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace clockProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockRadio clockRadio1 = new ClockRadio("5:30","95.7 FM",false,"3:00am");
+            clockRadio1.AlarmOn();
+            clockRadio1.GetAlarmTime();
+            clockRadio1.ChangeAlarmTime();
+            clockRadio1.GetAlarmTime();
+            clockRadio1.GetCurrentStaion();
+            clockRadio1.ChangeStation();
+            clockRadio1.GetCurrentStaion();
+            clockRadio1.AlarmOff();
+
+
+        }
+    }
+}
